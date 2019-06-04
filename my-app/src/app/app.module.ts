811433e0908cff5592c7867bf0ea01593f26f680
Jessica Carolina Rodriguez <jess.fresiamay@gmail.com>
2019-06-04 15:39:53 +0800

    Update app.module.ts

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -9,14 +9,7 @@ import { LoginComponent } from './user/login/login.component';
 import { AngularFireModule } from "@angular/fire";
 import { AngularFireAuthModule } from "@angular/fire/auth";
 
-const firebaseConfig = {
-    apiKey: "AIzaSyCgnj2hpqpp15rsluBSx9SHfqbMLAKyL1M",
-    authDomain: "angular-cashier-app.firebaseapp.com",
-    databaseURL: "https://angular-cashier-app.firebaseio.com",
-    projectId: "angular-cashier-app",
-    storageBucket: "angular-cashier-app.appspot.com",
-    messagingSenderId: "755750908137",
-    appId: "1:755750908137:web:4b37acc3e8e4635a"
+const firebaseConfig = { // get the config object in the Firebase Console 
 };
 
 @NgModule({
