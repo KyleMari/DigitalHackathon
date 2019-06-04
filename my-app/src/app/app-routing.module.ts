57d4eac0100720987d1d51e785dfb2cdeb8e4566
Jessica Carolina Rodriguez <malicdemj@google.com>
2019-06-04 14:41:52 +0800

    add login

--- my-app/src/app/app-routing.module.ts
+++ my-app/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './user/login/login.component';
+import { UserComponent } from './user/user.component';
 
-const routes: Routes = [];
+const  routes:  Routes  = [
+  {
+  path:  'user',
+  component:  UserComponent,
+  
+      children: [
+          { path:  'login',component:  LoginComponent}
+      ]
+    }
+  ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -3,14 +3,33 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserComponent } from './user/user.component';
+import { LoginComponent } from './user/login/login.component';
+
+import { AngularFireModule } from "@angular/fire";
+import { AngularFireAuthModule } from "@angular/fire/auth";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCgnj2hpqpp15rsluBSx9SHfqbMLAKyL1M",
+    authDomain: "angular-cashier-app.firebaseapp.com",
+    databaseURL: "https://angular-cashier-app.firebaseio.com",
+    projectId: "angular-cashier-app",
+    storageBucket: "angular-cashier-app.appspot.com",
+    messagingSenderId: "755750908137",
+    appId: "1:755750908137:web:4b37acc3e8e4635a"
+};
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
