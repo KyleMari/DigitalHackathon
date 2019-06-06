2b7f6c377e9397d2d2d8f7d4125e236df0cae86b
Jessica Carolina Rodriguez <malicdemj@google.com>
2019-06-07 01:04:33 +0800

    update codes

--- cashier-app/src/app/component/transactions/transactions.component.ts
+++ cashier-app/src/app/component/transactions/transactions.component.ts
@@ -1,15 +1,98 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {MatDatepickerInputEvent} from '@angular/material/datepicker';
 
 @Component({
   selector: 'app-transactions',
   templateUrl: './transactions.component.html',
   styleUrls: ['./transactions.component.scss']
 })
+
+
 export class TransactionsComponent implements OnInit {
 
-  constructor() { }
+  transactions: Transactions[] = [
+    {value: '150', viewValue: 'Shuttle'},
+    {value: '300', viewValue: 'LostID'}
+  ];
 
-  ngOnInit() {
+  amount: string;
+  transaction: string;
+  trasactionType: string;
+  events: string[] = [];
+  startDate: Date;
+  endDate: Date;
+  selectShuttle: boolean = true;
+  reason: string;
+  confirmPayment: boolean = false;
+  summaryPayment: object;
+  confirm: boolean = false;
+
+  firstFormGroup: FormGroup;
+  secondFormGroup: FormGroup;
+
+  constructor(private _formBuilder: FormBuilder) { 
+    this.confirm = false;
   }
 
+  ngOnInit() {
+    this.firstFormGroup = this._formBuilder.group({
+      firstCtrl: ['', Validators.required]
+    });
+    this.secondFormGroup = this._formBuilder.group({
+      secondCtrl: ['', Validators.required]
+    });
+
+  }
+
+  transactionLevel(event) {
+    this.amount = event.value.value;
+    this.transaction = event.value.value;
+    this.trasactionType = event.value.viewValue;
+    if (this.trasactionType === "Shuttle") {
+      this.selectShuttle = true;
+    } else {
+      this.selectShuttle = false
+    }
+  }
+
+  addStartEvent(type: string, event: MatDatepickerInputEvent<Date>) {
+    this.startDate = event.value;
+    this.events.push(`${type}: ${event.value}`);
+  }
+
+  addEndEvent(type: string, event: MatDatepickerInputEvent<Date>) {
+    this.endDate = event.value;
+    this.events.push(`${type}: ${event.value}`);
+  }
+
+  confirmTransaction() {
+    this.confirm = true;
+    let reqBody;
+    if (this.trasactionType == "Shuttle") {
+       reqBody = {
+        'accountID': "1234567",
+        'transaction': this.transaction,
+        'amount': this.amount,
+        'startDate': this.startDate,
+        'end': this.endDate
+      }
+    } else {
+       reqBody = {
+        'accountID': "1234567",
+        'transaction': this.transaction,
+        'amount': this.amount,
+        'reason': this.reason
+       }
+     }
+     this.summaryPayment = reqBody;
+     reqBody = [];
+   }
 }
+
+
+export interface Transactions {
+  value: string;
+  viewValue: string;
+}
+
--- cashier-app/src/app/material.module.ts
+++ cashier-app/src/app/material.module.ts
@@ -1,25 +1,77 @@
 import { NgModule } from '@angular/core';
-import { MatButtonModule } from '@angular/material/button';
-import { MatCardModule } from '@angular/material/card';
-import { MatCheckboxModule } from '@angular/material/checkbox';
-import { MatExpansionModule } from '@angular/material/expansion';
-import { MatIconModule } from '@angular/material/icon';
-import { MatListModule } from '@angular/material/list';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatInputModule } from '@angular/material/input';
-
-const MATERIAL_MODULES = [
+import { MatAutocompleteModule,
+  MatBadgeModule,
+  MatBottomSheetModule,
   MatButtonModule,
+  MatButtonToggleModule,
   MatCardModule,
   MatCheckboxModule,
+  MatChipsModule,
+  MatDatepickerModule,
+  MatDialogModule,
+  MatDividerModule,
   MatExpansionModule,
+  MatGridListModule,
   MatIconModule,
-  MatListModule,
-  MatSidenavModule,
-  MatToolbarModule,
   MatInputModule,
-  MatButtonModule
+  MatListModule,
+  MatMenuModule,
+  MatNativeDateModule,
+  MatPaginatorModule,
+  MatProgressBarModule,
+  MatProgressSpinnerModule,
+  MatRadioModule,
+  MatRippleModule,
+  MatSelectModule,
+  MatSidenavModule,
+  MatSliderModule,
+  MatSlideToggleModule,
+  MatSnackBarModule,
+  MatSortModule,
+  MatStepperModule,
+  MatTableModule,
+  MatTabsModule,
+  MatToolbarModule,
+  MatTooltipModule,
+  MatTreeModule } from '@angular/material';
+
+
+const MATERIAL_MODULES = [
+  MatAutocompleteModule,
+  MatBadgeModule,
+  MatBottomSheetModule,
+  MatButtonModule,
+  MatButtonToggleModule,
+  MatCardModule,
+  MatCheckboxModule,
+  MatChipsModule,
+  MatDatepickerModule,
+  MatDialogModule,
+  MatDividerModule,
+  MatExpansionModule,
+  MatGridListModule,
+  MatIconModule,
+  MatInputModule,
+  MatListModule,
+  MatMenuModule,
+  MatNativeDateModule,
+  MatPaginatorModule,
+  MatProgressBarModule,
+  MatProgressSpinnerModule,
+  MatRadioModule,
+  MatRippleModule,
+  MatSelectModule,
+  MatSidenavModule,
+  MatSliderModule,
+  MatSlideToggleModule,
+  MatSnackBarModule,
+  MatSortModule,
+  MatStepperModule,
+  MatTableModule,
+  MatTabsModule,
+  MatToolbarModule,
+  MatTooltipModule,
+  MatTreeModule,
 ];
 
 @NgModule({
