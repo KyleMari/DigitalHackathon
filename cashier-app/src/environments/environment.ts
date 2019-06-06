cab8bbe9f8e02790d78076fc73000875e518c54b
Kyle Mari Torralba <ktorralba@google.com>
2019-06-06 19:22:28 +0800

    ui-and-login

--- cashier-app/src/environments/environment.ts
+++ cashier-app/src/environments/environment.ts
@@ -5,13 +5,6 @@
 export const environment = {
   production: false,
   firebase: { 
-    apiKey: "AIzaSyCgnj2hpqpp15rsluBSx9SHfqbMLAKyL1M",
-    authDomain: "angular-cashier-app.firebaseapp.com",
-    databaseURL: "https://angular-cashier-app.firebaseio.com",
-    projectId: "angular-cashier-app",
-    storageBucket: "angular-cashier-app.appspot.com",
-    messagingSenderId: "755750908137",
-    appId: "1:755750908137:web:4b37acc3e8e4635a"
   }
 };
 
